7a7c9c4393259ce22bf8ebeda58d42f151dc8ad8
michaelgailling <michaelgailling@users.noreply.github.com>
2020-02-14 11:46:21 -0500

    Tinkering
    Added ID prefab

---
+++ UnityProject/Assets/DataDisk.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Mirror;
+
+public class DataDisk : NetworkBehaviour, IClientInteractable<HandActivate>, IClientInteractable<HandApply>
+{
+	public bool Interact(HandActivate interaction)
+	{
+		throw new NotImplementedException();
+	}
+
+	public bool Interact(HandApply interaction)
+	{
+		throw new NotImplementedException();
+	}
+
+	// Start is called before the first frame update
+	void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
